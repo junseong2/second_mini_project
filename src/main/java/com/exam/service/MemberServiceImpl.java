af2fdc28846359ba32fcb522f8374a8c30135de8
Sojung Lee <dlthwjd1118@gmail.com>
2025-02-06 13:31:32 +0900

    원래준성파일

---
+++ src/main/java/com/exam/service/MemberServiceImpl.java
@@ -0,0 +1,49 @@
+package com.exam.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Service;
+
+import com.exam.dto.GoodsDTO;
+import com.exam.dto.MemberDTO;
+import com.exam.mapper.GoodsMapper;
+import com.exam.mapper.MemberMapper;
+
+//service 부분이니까 @Service 어노테이션 주기
+@Service
+public class MemberServiceImpl implements MemberService{
+
+	MemberMapper mapper;
+	//생성자로 주입
+	public MemberServiceImpl(MemberMapper mapper) {
+		this.mapper = mapper;
+	}
+	@Override
+	public MemberDTO idCheck(String userid) {
+		return mapper.idCheck(userid);
+	}
+	@Override
+	public int memberAdd(MemberDTO dto) {
+		return mapper.memberAdd(dto);
+	}
+	@Override
+	public MemberDTO login(Map<String, String> map) {
+		return mapper.login(map);
+	}
+	@Override
+	public MemberDTO mypage(String userid) {
+		return mapper.mypage(userid);
+	}
+	@Override
+	public String findid(MemberDTO dto) {
+		return mapper.findid(dto);
+	}
+	@Override
+	public String findpw(MemberDTO dto) {
+		return mapper.findpw(dto);
+	}
+
+}
