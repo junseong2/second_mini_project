dd68fbf9827eac49df161c7bb5ff52fb738c15b3
Sojung Lee <dlthwjd1118@gmail.com>
2025-02-06 13:31:32 +0900

    원래준성파일

---
+++ src/main/java/com/exam/service/CartService.java
@@ -0,0 +1,16 @@
+package com.exam.service;
+
+import java.util.List;
+
+import com.exam.dto.CartDTO;
+
+
+
+public interface CartService {
+
+	public int cartAdd(CartDTO dto);
+	public List<CartDTO> cartList(String userid);
+	public int cartDelete(int num);
+	public int cartDeleteAll(List<String> list);
+	
+}
