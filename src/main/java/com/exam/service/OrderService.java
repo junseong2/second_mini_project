eb6264d3efc815480d0072e21b9c9a376997c89a
junseong2 <ljs228811@naver.com>
2025-02-06 14:06:51 +0900

    일단 어제꺼까지 완성

--- src/main/java/com/exam/service/OrderService.java
+++ src/main/java/com/exam/service/OrderService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.exam.dto.CartDTO;
 import com.exam.dto.MemberDTO;
+import com.exam.dto.OrderDTO;
 
 
 
@@ -13,4 +14,6 @@ public interface OrderService {
 	
 	public MemberDTO orderConfirmMember(String userid);
 	
+	public int orderDone(OrderDTO dto);
+	public int AfterOrder(int num);
 }
