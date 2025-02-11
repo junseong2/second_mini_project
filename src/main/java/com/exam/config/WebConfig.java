868113198cd29a777fe5d0ddd3004f5758f3021e
Sojung Lee <dlthwjd1118@gmail.com>
2025-02-11 10:35:49 +0900

    [Modified] 준성 사진 파일확장자명 수정, WebConfig.java에 /buyGooods 추가

--- src/main/java/com/exam/config/WebConfig.java
+++ src/main/java/com/exam/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
       
       //registry.addInterceptor(xxx);
       // /cart* 로 퉁쳐도됨
-      registry.addInterceptor(xxx).addPathPatterns("/logout","/mypage","/cartAdd","/cartList","/cartDelete","/cartDeleteAll","/orderConfirm");
+      registry.addInterceptor(xxx).addPathPatterns("/logout","/mypage","/cartAdd","/cartList","/cartDelete","/cartDeleteAll","/orderConfirm","/buyGoods");
       
    }
    
