d1b680fe9bdc21f322dd5f44fd998a630973c361
junseong2 <ljs228811@naver.com>
2025-02-11 16:16:26 +0900

    정렬완성

--- src/main/java/com/exam/mapper/GoodsMapper.java
+++ src/main/java/com/exam/mapper/GoodsMapper.java
@@ -3,16 +3,24 @@ package com.exam.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.exam.dto.GoodsDTO;
 
 @Mapper
 public interface GoodsMapper {
-	
+
 	public List<GoodsDTO> goodsList(String gCategory);
+
 	public GoodsDTO goodsRetrieve(String gCode);
 
 	public int goodsAdd(GoodsDTO dto);
-  public List<GoodsDTO> search(String gName);
+
+	public List<GoodsDTO> search(String gName);
+
+	public List<GoodsDTO> getAllGoodsByCategory(String gCategory);
+
+
+
 
 }
